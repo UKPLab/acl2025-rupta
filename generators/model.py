a1186cf92b742ffc7f5a2e9ccd3ab823bca5d70a
jjyuhub <jyu290@student.vu.nl>
2025-02-27 16:30:54 +0100

    Update model.py

--- generators/model.py
+++ generators/model.py
@@ -184,17 +184,9 @@ class GPTChat(ModelBase):
 class GPT4(GPTChat):
     def __init__(self, name):
         super().__init__(name)
-        if name == "gpt-4":
-            self.endpoint = credentials.gpt4_endpoint
-            self.api_key = credentials.gpt4_api_key
-            self.api_version = credentials.gpt4_api_version
-        else:
-            assert name == "gpt4-turbo-128k" or name == "gpt-4-turbo-preview"
-            self.endpoint = credentials.gpt4_tb_endpoint
-            self.api_key = credentials.gpt4_tb_api_key
-            self.api_version = credentials.gpt4_tb_api_version
+
         self.client = OpenAI(
-            api_key=self.api_key
+            api_key=self.OPENAI_API_KEY
         )
         
         # self.client = OpenAI(
